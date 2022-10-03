451237b5b3b5755b635a0d830d4a3d3f275c6058
alfonsouzcategui2@gmail.com <alfonsouzcategui2@gmail.com>
2022-10-02 20:14:53 -0500

    import data

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';import React from 'react';
 
 const Login = () => {
     const [loginSignup, setLoginSignup] = useState(true)
