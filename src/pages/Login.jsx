0cd88875c14a21cc4cdb2c5c4d827f02710e96a3
alfonsouzcategui2@gmail.com <alfonsouzcategui2@gmail.com>
2022-10-02 20:12:25 -0500

    update login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
 
 const Login = () => {
+    const [loginSignup, setLoginSignup] = useState(true)
+    const { register, handleSubmit, reset } = useForm()
+    
+    const submit = userData => {
+      console.log(userData)
+    }
+    
+    const userRegister = newUser => {
+      console.log(newUser)
+    }
+    
+    const changeSection = () => {
+      setLoginSignup(!loginSignup)
+    }
+  
     return (
-        <div>
-            <h2>Login</h2>
+        <div className='form-container'>
+          {
+            loginSignup ? (
+              <form onSubmit={handleSubmit=(submit())}>
+              <h2>Welcome! enter you email and password to continue</h2>
+              <article>
+                <b>Test data</b>
+                <p>mason@gmail.com</p>
+                <p>mason1234</p>
+              </article>
+            <input type='email' placeholder='Email' {...register('email')}/>
+            <input type='password' placeholder='Password' {...register('pass')}/>
+            <button>Login</button>
+             <p>Don't have an account?<span onClick={() => changeSection()}>Sign up</span></p>
+          </form>
+            ) : (
+              <form onSubmit={handleSubmit=(userRegister())}>
+              <h2>Sign up</h2>
+            <input type='email' placeholder='Email' {...register('email')}/>
+            <input type='text' placeholder='Fisrt Name' {...register('firstName')}/>
+            <input type='text' placeholder='Last Name' {...register('lastName')}/>
+            <input type='password' placeholder='Password' {...register('pass')}/>
+            <input type='text' placeholder='Phone Number' {...register('phone')}/>
+            <button>Sign up</button>
+             <p>Already have an account?<span onClick={() => changeSection()}>Login</span></p>
+          </form>
+            )
+          }
         </div>
     );
 };
